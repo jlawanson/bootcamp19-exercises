9b7bcf3fa57282b11d970a4ad3189155ec32d266
Jordan Lawanson <jlawanson@college.harvard.edu>
2019-01-09 12:56:47 -0500

    3_AM solutions

--- 3_AM/index.js
+++ 3_AM/index.js
@@ -1,11 +1,22 @@
 const express = require("express");
+const path = require("path");
 const app = express();
-
-app.use(express.static(__dirname + "/View"));
-app.use(express.static(__dirname + "/Script"));
+app.use(express.static('public'))
 
 app.get("/", (req, res) => {
-  res.sendFile("index.html");
+  res.sendFile(path.join(__dirname + "/index.html"));
+});
+
+app.get("/miri", (req, res) => {
+  res.sendFile(path.join(__dirname + "/miri.html"));
+});
+
+app.get("/adiah", (req, res) => {
+  res.sendFile(path.join(__dirname + "/adiah.html"));
+});
+
+app.get("/jesus", (req, res) => {
+  res.sendFile(path.join(__dirname + "/jesus.html"));
 });
 
 app.listen(3000);
